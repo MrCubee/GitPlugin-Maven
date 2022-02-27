be1ae94dd427607566a6bb336b0d7e71a5123096
MrCubee <contact@mrcubee.fr>
2022-02-27 19:04:27 +0100

    Clean code in Parse.java.

--- src/main/java/fr/mrcubee/maven/git/Parse.java
+++ src/main/java/fr/mrcubee/maven/git/Parse.java
@@ -20,21 +20,79 @@ import java.util.Set;
 @Mojo(name = "parse", threadSafe = true, requiresProject = true, defaultPhase = LifecyclePhase.INITIALIZE)
 public class Parse extends AbstractMojo {
 
+    public static final String PROPERTY_BRANCH_NAME = "git.branch.name";
+    public static final String PROPERTY_BRANCH_FULL_NAME = "git.branch.name_full";
+    public static final String PROPERTY_BRANCH_AUTHORS = "git.branch.authors";
+    public static final String PROPERTY_LAST_COMMIT_SHA1 = "git.commit.last.sha1";
+    public static final String PROPERTY_LAST_COMMIT_SHORT_SHA1 = "git.commit.last.sha1_short";
+    public static final String PROPERTY_LAST_COMMIT_AUTHOR = "git.commit.last.author";
+
     @Parameter(property = "project", readonly = false)
     private MavenProject project;
 
-    private static String personIndentsToString(final Set<PersonIdent> personIdents) {
+    @Parameter(property = PROPERTY_BRANCH_NAME, readonly = false, defaultValue = "none")
+    private String branchName;
+
+    @Parameter(property = PROPERTY_BRANCH_FULL_NAME, readonly = false, defaultValue = "none")
+    private String branchFullName;
+
+    @Parameter(property = PROPERTY_BRANCH_AUTHORS, readonly = false)
+    private String branchAuthors;
+
+    @Parameter(property = PROPERTY_LAST_COMMIT_SHA1, readonly = false, defaultValue = "none")
+    private String lastCommitSha1;
+
+    @Parameter(property = PROPERTY_LAST_COMMIT_SHORT_SHA1, readonly = false, defaultValue = "none")
+    private String lastCommitSha1Short;
+
+    @Parameter(property = PROPERTY_LAST_COMMIT_AUTHOR, readonly = false, defaultValue = "nobody")
+    private String lastCommitAuthor;
+
+    private static String personIndentsToString(final Set<String> authorNames) {
         final StringBuilder strBuilder;
 
-        if (personIdents == null || personIdents.isEmpty())
-            return "nobody";
+        if (authorNames == null || authorNames.isEmpty())
+            return "";
         strBuilder = new StringBuilder();
-        for (PersonIdent personIdent : personIdents) {
+        for (String name : authorNames) {
             if (strBuilder.length() != 0)
                 strBuilder.append(", ");
-            strBuilder.append(personIdent.getName());
+            strBuilder.append(name);
         }
         return strBuilder.toString();
+    }
+
+    private void registerBranchProperties(final Properties properties, final Repository repository) throws IOException {
+        final RevWalk revWalk;
+        final Set<String> authors;
+        RevCommit revCommit;
+        PersonIdent author;
+
+        if (properties == null || repository == null)
+            return;
+
+        revWalk = new RevWalk(repository);
+        authors = new HashSet<String>();
+        properties.setProperty(PROPERTY_BRANCH_NAME, this.branchName = repository.getBranch());
+        properties.setProperty(PROPERTY_BRANCH_FULL_NAME, this.branchFullName = repository.getFullBranch());
+        for (Ref ref : repository.getRefDatabase().getRefs()) {
+            revCommit = revWalk.parseCommit(ref.getObjectId());
+            revWalk.markStart(revCommit);
+        }
+        for (RevCommit commit : revWalk) {
+            author = new CraftPersonIndent(commit.getAuthorIdent());
+            getLog().info("Parsing " + commit.getName() + " (author: " + author.toExternalString() + ") ...");
+            authors.add(author.getName());
+        }
+        properties.setProperty(PROPERTY_BRANCH_AUTHORS, this.branchAuthors = personIndentsToString(authors));
+    }
+
+    private void registerLastCommitProperties(final Properties properties, final RevCommit revCommit) {
+        if (properties == null || revCommit == null)
+            return;
+        properties.setProperty(PROPERTY_LAST_COMMIT_AUTHOR, this.lastCommitAuthor = revCommit.getAuthorIdent().getName());
+        properties.setProperty(PROPERTY_LAST_COMMIT_SHA1, this.lastCommitSha1 = revCommit.getName());
+        properties.setProperty(PROPERTY_LAST_COMMIT_SHORT_SHA1, this.lastCommitSha1Short = this.lastCommitSha1.substring(0, 7));
     }
 
     @Override
@@ -42,12 +100,7 @@ public class Parse extends AbstractMojo {
         final Properties properties;
         final Git git;
         final Repository repository;
-        final RevWalk revWalk;
         final ObjectId lastCommitId;
-        final String lastCommitSha1;
-        final Set<PersonIdent> authors;
-        PersonIdent author;
-        RevCommit revCommit;
 
         if (project == null) {
             getLog().error("Maven project is null.");
@@ -62,33 +115,13 @@ public class Parse extends AbstractMojo {
                 getLog().error("Git repository not found.");
                 return;
             }
-            revWalk = new RevWalk(repository);
-            authors = new HashSet<PersonIdent>();
-            properties.setProperty("git.branch.name", repository.getBranch());
-            properties.setProperty("git.branch.name_full", repository.getFullBranch());
-            for (Ref ref : repository.getRefDatabase().getRefs()) {
-                revCommit = revWalk.parseCommit(ref.getObjectId());
-                revWalk.markStart(revCommit);
-            }
-            for (RevCommit commit : revWalk) {
-                author = new CraftPersonIndent(commit.getAuthorIdent());
-                getLog().info("Parsing " + commit.getName() + " (author: " + author.toExternalString() + ") ...");
-                authors.add(author);
-            }
-            properties.setProperty("git.branch.authors", personIndentsToString(authors));
+            registerBranchProperties(properties, repository);
             lastCommitId = repository.resolve(Constants.HEAD);
             if (lastCommitId == null) {
-                properties.setProperty("git.commit.last.sha1", "none");
-                properties.setProperty("git.commit.last.sha1_short", "none");
-                properties.setProperty("git.commit.last.author", "nobody");
                 getLog().error("Commit not found.");
                 return;
             }
-            revCommit = revWalk.parseCommit(lastCommitId);
-            properties.setProperty("git.commit.last.author", revCommit.getAuthorIdent().getName());
-            lastCommitSha1 = lastCommitId.getName();
-            properties.setProperty("git.commit.last.sha1", lastCommitSha1);
-            properties.setProperty("git.commit.last.sha1_short", lastCommitSha1.substring(0, 7));
+            registerLastCommitProperties(properties, new RevWalk(repository).parseCommit(lastCommitId));
         } catch (IOException exception) {
             getLog().error(exception);
         }
